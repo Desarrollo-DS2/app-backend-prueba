d3beffc4ba7b013bab608ed7792f37c42e7e49df
Samuel Galindo <itzsaky@outlook.com>
2024-03-07 19:11:48 -0500

    prueba10

--- appbackend/settings.py
+++ appbackend/settings.py
@@ -81,9 +81,15 @@ WSGI_APPLICATION = 'appbackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': { dj_database_url.config(
         default=config('DATABASE_URL')
     )
+    }, 
+    
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
 }
 
 
